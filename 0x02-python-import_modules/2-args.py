0a5bdf75982b9bf4401a352c847a51a08426d691
ErosAlx <erosalx1@gmail.com>
2023-11-30 18:51:04 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     arg_c = len(sys.argv[1:])
     if arg_c == 1:
-        print(arg_c, "argument")
+        print(arg_c, "argument:")
     elif arg_c == 0:
-        print(arg_c, "argument")
+        print(arg_c, "arguments.")
     else:
-        print(arg_c, "arguments")
+        print(arg_c, "arguments:")
     for i in range(arg_c):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
